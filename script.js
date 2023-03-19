83093dcab4bd557876b774b8a10deb92ef7d2fd4
Yonah <yonahveffer@gmail.com>
2023-03-19 21:35:36 +0200

    Updated program with two versions

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+//Create a program that randomly returns a sentence made up of three pieces of data. 
+
+///// VERSION ONE /////
+
+//Step 1 = Store message components.
 
   let messageComponents = {
     signs: ['moon', 'sun', 'stars', 'earth'],
@@ -15,3 +20,25 @@
   console.log(`your luck today is:${randomLuckState}`);
   console.log(`You should: ${randomYouShould}`);
   
+
+  
+  //////VERSION TWO /////
+  
+  //Step 1 = Store message components
+
+const dayOfWeek = ['Sunday','Monday','Tuesday','Wednsday','Thursday','Friday','Shabbos'];
+const howYourFeeling = ['happy','sad','excited','mad','curious'];
+const whatYouShouldDo = ['go outside', 'play ball', 'go for a walk','learn'];
+
+// Step 2 = Get a random string from each variable and store it
+
+
+let randomdayOfWeek = dayOfWeek[Math.floor(Math.random() * dayOfWeek.length)]
+let randomhowYourFeeling = howYourFeeling[Math.floor(Math.random() * howYourFeeling.length)]
+let randomwhatYouShouldDo = whatYouShouldDo[Math.floor(Math.random() * whatYouShouldDo.length)]
+
+// step 3 = declare a new variable with final message.
+
+let finalMessage = [`Today is ${randomdayOfWeek}, and your feeling ${randomhowYourFeeling}, therfore your should ${randomwhatYouShouldDo}.`]
+
+console.log(finalMessage)
